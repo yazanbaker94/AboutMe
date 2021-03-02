9fef126bfb5992009ff18b232cc3ad04b11b169c
Yazan Baker <yazanbaker94@hotmail.com>
2021-03-02 16:49:52 +0200

    added question 6/7/counter

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 let nameQ = prompt("What's Your Name?");
 alert("Welcome back, " + nameQ);
 
+let counter = 0;
 
 alert("please only answer 'Yes', 'No', 'y' OR 'n'");
 let firstQ = prompt("Are United States elections next year?");
@@ -11,7 +12,9 @@ let firstA = firstQ.toLocaleLowerCase();
 console.log(firstA);
 
 if(firstA === 'no' || firstA === 'n'){
-    alert("Your answer is correct!")
+    alert("Your answer is correct!");
+    counter++;
+    console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
 }
@@ -23,6 +26,8 @@ console.log(secondA);
 
 if(secondA === 'no' || secondA === 'n'){
     alert("Your answer is correct!")
+    counter++;
+    console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
 }
@@ -33,6 +38,8 @@ console.log(thirdA);
 
 if(thirdA === 'no' || thirdA === 'n'){
     alert("Your answer is correct!")
+    counter++;
+    console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
 }
@@ -42,7 +49,9 @@ let fourthA = fourthQ.toLocaleLowerCase();
 console.log(fourthA);
 
 if(fourthA === 'yes' || fourthA === 'y'){
-    alert("Your answer is correct!")
+    alert("Your answer is correct!");
+    counter++;
+    console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
 }
@@ -52,10 +61,75 @@ let fifthA = fifthQ.toLocaleLowerCase();
 console.log(fifthA);
 
 if(fifthA === 'yes' || fifthA === 'y'){
-    alert("Your answer is correct!")
+    alert("Your answer is correct!");
+    counter++;
+    console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time, " + nameQ + " !");
 }
+
+let correctNumber = 87;
+let parsedCorrecNumber = parseInt(correctNumber);
+let userAttempt = 4;
+
+// do {
+//     let userGuess = prompt("Please enter a number between 50 and 100: ");
+//     if(userGuess == parsedCorrecNumber){
+//         alert("Correct answer");
+//     } else if(userGuess!=parsedCorrecNumber) {
+//         alert("wrong answer");
+//     }
+//     userAttempt-=1;
+//   } while(userAttempt = 0 );
+
+
+while(userAttempt){
+    let userGuess = prompt("Please enter a number between 50 and 100: ");
+    if(userGuess == parsedCorrecNumber) {
+        alert("Correct!");
+        counter++;
+        console.log(counter);
+        break;
+    } else if(userGuess > parsedCorrecNumber){
+        alert("Too high");
+
+    } else if(userGuess < parsedCorrecNumber){
+        alert("Too low");
+    } 
+    
+    
+    userAttempt-=1;
+
+}
+
+alert("correct answer is 87!");
+
+
+let userAttempt2 = 6;
+let correctAnswers = ["Irbid", "Amman", "Zarqa", "Mafraq"];
+while(userAttempt2){
+    let userQuestion= prompt("Write down 5 cities inside Jordan: ");
+    if(correctAnswers[0] == userQuestion || correctAnswers[1] == userQuestion || correctAnswers[2] == userQuestion || correctAnswers[3] == userQuestion) {
+        alert("Correct!");
+        counter++;
+        console.log(counter);
+        break;
+    } else {
+        alert('please try again');
+    }   
+    
+    userAttempt2-=1;
+
+}
+
+
+
+
+alert("You have scored " + counter + " out of 7");
+
+
+
+
 
 
 
