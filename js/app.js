3255ff702e544a69778a69e87bca974d962a9b2a
saif hamaydeh <saif.ahmad12395@gmail.com>
2021-03-04 16:42:45 +0200

    Add functions

--- js/app.js
+++ js/app.js
@@ -1,76 +1,92 @@
 'use strict';
 
+let counter = 0;
+function newFunctionYes(){
+    if(userInput === 'yes' || userInput === 'y')
+        alert("Your answer is correct!");
+        counter++;
+        console.log(counter);
+
+}
+function newFunctionNo(){
+    if(userInput === 'no' || userInput === 'n'){
+        alert("Your answer is correct!");
+        counter++;
+        console.log(counter);
+    } else {
+        alert("Your answer is wrong. Good luck next time!")
+    }
+    
+
+}
+
 
 let nameQ = prompt("What's Your Name?");
 alert("Welcome back, " + nameQ);
 
-let counter = 0;
 
 alert("please only answer 'Yes', 'No', 'y' OR 'n'");
-let firstQ = prompt("Are United States elections next year?");
-let firstA = firstQ.toLocaleLowerCase();
-console.log(firstA);
-
-if(firstA === 'no' || firstA === 'n'){
+let user = prompt("Are United States elections next year?");
+let userInput = user.toLocaleLowerCase();
+console.log(userInput);
+newFunctionNo();
+/*if(firstA === 'no' || firstA === 'n'){
     alert("Your answer is correct!");
     counter++;
     console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
-}
+}*/
 
 
-let secondQ = prompt("Is the Jordanian Currency Dollars?");
-let secondA = secondQ.toLocaleLowerCase();
-console.log(secondA);
-
-if(secondA === 'no' || secondA === 'n'){
+ user = prompt("Is the Jordanian Currency Dollars?");
+ userInput = user.toLocaleLowerCase();
+console.log(userInput);
+newFunctionNo();
+/*if(secondA === 'no' || secondA === 'n'){
     alert("Your answer is correct!")
     counter++;
     console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
-}
+}*/
 
-let thirdQ = prompt("Is Iraq in Africa?");
-let thirdA = thirdQ.toLocaleLowerCase();
-console.log(thirdA);
-
-if(thirdA === 'no' || thirdA === 'n'){
+user = prompt("Is Iraq in Africa?");
+ userInput = user.toLocaleLowerCase();
+console.log(userInput);
+newFunctionNo();
+/*if(thirdA === 'no' || thirdA === 'n'){
     alert("Your answer is correct!")
     counter++;
     console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
-}
+}*/
 
-let fourthQ = prompt("Is Russia the biggest country in the world?");
-let fourthA = fourthQ.toLocaleLowerCase();
-console.log(fourthA);
-
-if(fourthA === 'yes' || fourthA === 'y'){
+ user = prompt("Is Russia the biggest country in the world?");
+ userInput = user.toLocaleLowerCase();
+console.log(userInput);
+newFunctionYes();
+/*if(fourthA === 'yes' || fourthA === 'y'){
     alert("Your answer is correct!");
     counter++;
     console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time!")
-}
+}*/
 
-let fifthQ = prompt("Is Lebanon in Asia?");
-let fifthA = fifthQ.toLocaleLowerCase();
-console.log(fifthA);
-
-if(fifthA === 'yes' || fifthA === 'y'){
+user = prompt("Is Lebanon in Asia?");
+userInput = user.toLocaleLowerCase();
+console.log(userInput);
+newFunctionYes();
+/*if(fifthA === 'yes' || fifthA === 'y'){
     alert("Your answer is correct!");
     counter++;
     console.log(counter);
 } else {
     alert("Your answer is wrong. Good luck next time, " + nameQ + " !");
-}
+}*/
 
-let correctNumber = 87;
-let parsedCorrecNumber = parseInt(correctNumber);
-let userAttempt = 4;
 
 // do {
 //     let userGuess = prompt("Please enter a number between 50 and 100: ");
@@ -81,9 +97,13 @@ let userAttempt = 4;
 //     }
 //     userAttempt-=1;
 //   } while(userAttempt = 0 );
-
-
-while(userAttempt){
+let correctNumber = 87;
+let parsedCorrecNumber = parseInt(correctNumber);
+let userAttempt = 4;
+function a(){
+    while(userAttempt){
+    
+        
     let userGuess = prompt("Please enter a number between 50 and 100: ");
     if(userGuess == parsedCorrecNumber) {
         alert("Correct!");
@@ -98,32 +118,40 @@ while(userAttempt){
     } 
     
     
-    userAttempt-=1;
+    userAttempt-=1;  
+}}
 
-}
+
+
+
+a();
+
+
 
 alert("correct answer is 87!");
-
-
 let userAttempt2 = 6;
 let correctAnswers = ["Irbid", "Amman", "Zarqa", "Mafraq"];
-while(userAttempt2){
-    let userQuestion= prompt("Write down 5 cities inside Jordan: ");
-    if(correctAnswers[0] == userQuestion || correctAnswers[1] == userQuestion || correctAnswers[2] == userQuestion || correctAnswers[3] == userQuestion) {
-        alert("Correct!");
-        counter++;
-        console.log(counter);
-        break;
-    } else {
-        alert('please try again');
-    }   
+function b(){
+    while(userAttempt2){
+        let userQuestion= prompt("Write down 5 cities inside Jordan: ");
+        if(correctAnswers[0] == userQuestion || correctAnswers[1] == userQuestion || correctAnswers[2] == userQuestion || correctAnswers[3] == userQuestion) {
+            alert("Correct!");
+            counter++;
+            console.log(counter);
+            break;
+        } else {
+            alert('please try again');
+        }   
+        
+        userAttempt2-=1;
     
-    userAttempt2-=1;
-
+    }
+    
+    
 }
 
 
-
+b();
 
 alert("You have scored " + counter + " out of 7");
 
